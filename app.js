b68cc4ececcc1742d709455c7b54a54a7128fe6f
codealtgeek <codealtgeek@gmail.com>
2017-08-10 14:19:04 -0400

    Finished

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 /* The four rules for 'this';
 * in your own words. explain the four rules for the "this" keyword below.
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.  Rule number one means that the this points globally..
+* 2. Rule number 2 means any method before the . on an object call is "this"
+* 3. I think this means that the this points to a new unique object
+* 4. This means that you can use call or apply to change the state of this
 * write out a code example of each explanation above
 */
 
 // First Rule
+function greeted(name){
+  console.log('hello' + this );
+  console.log(this);
 
+}
+greeted('Ron');
 // Second Rule
-
+function Name(options){
+this.name=options.name;
+const sayName = function(){console.log(this,name);
+}
+const myName = Name({name: 'Ron' });
 // Third Rule
+function myName(name) {
+    
+    this.name = name;
+    this.speak = function() {
+        console.log(this.name);
+        console.log(this);
+    }
+};
+
 
 // Fourth Rule * you may want to use your third rule's example to accomplish this
+const newName = new Name({
+    name: 'joe'
+});
 
+newName.call(myName);
 // explain closure 
+//from what I understand, closure is the ability for a function to grab data outside of its enclosure
 
 function foo () {
-  console.log(this); // what does this point to?
+  console.log(this); // what does this point to? 
 };
 
 const counterFunction = () => {
   // this code is broken. figure out why, and tell us where the closure is when you fix it
+  let count = 0;
   const changeCount = (value) => {
     count += value;
   };
@@ -37,6 +61,7 @@ const counterFunction = () => {
       return count;
     }
   }
+}
 };
 const counter = counterFunction();
 counter.increment();
@@ -52,16 +77,36 @@ console.log(counter.total());
   // your options object should have "make", "model", "year" properties on it
   // assign these properties you pass in with options to the constructors 'this' object.
   // add a speak() method to your object that when called will log out the car's make model and year.
+function Car(options){
+this.make = options.make;
+this.model = options.model;
+this.year = options.year;
 
+this.speak = function(){
+  return `${this.make} ${this.model} ${this.year} `;
+}
+}
 // when you're done un comment the next few lines and run the file here in node `node app.js`.
 
-// const herby = new Car({make: 'Volkswagen', model: 'Beetle', year: '1963'});
-// console.log(herby.speak());
-// const goldfinger = new Car({make: 'Aston Martin', model: 'DB5', year: '1964'});
-// console.log(goldfinger.speak());
+//const herby = new Car({make: 'Volkswagen', model: 'Beetle', year: '1963'});
+//console.log(herby.speak());
+//const goldfinger = new Car({make: 'Aston Martin', model: 'DB5', year: '1964'});
+//console.log(goldfinger.speak());
 
 
 // once you get done with this, redo it all using the class keyword and a constructor function. 
+class Car {
+constructor(options){
+this.make = options.make;
+this.model = options.model;
+this.year = options.year;
+
+
+}
+speak(){
+  return ` ${this.make} ${this.model} ${this.year} `;
+}
+}
 
 // extra credit
 
@@ -75,7 +120,13 @@ while(n >= 1) {
 }
 // write a function called countDown that does the exact same thing as above, but calls itself until it can't anymore.
   // hint-> your base case will look like the logic in the while loop.
+let n = 10;
+const countDown = () => {
+console.log(n);
+n--;
+if(n > 0){
+  countDown();
+}
+}
 
-
-
-
+countDown();
