c432222eb29cc933c9851326d82dca0c3ced28f6
Randall Whitlock <randall.allen23@gmail.com>
2017-08-10 22:43:25 -0400

    Completed Advanced JavaScript mini sprint

--- app.js
+++ app.js
@@ -1,30 +1,71 @@
 /* The four rules for 'this';
-* in your own words. explain the four rules for the "this" keyword below.
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-* write out a code example of each explanation above
+ * in your own words. explain the four rules for the "this" keyword below.
+ * 1. When this is used inside of a globally scoped function, this refers to the window object.
+ * 2. When a method is called on an object, the object that precedes the dot is 'this'.
+ * 3. In the context of constructor functions, 'this' refers to the object that is instantiated by the constructor function,
+ * not the constructor function itself.
+ * 4. This is explicitly defined through the use of the 'call' or 'apply' methods.
+ * write out a code example of each explanation above
 */
 
 // First Rule
+function genericFunction() {
+  console.log(this);
+}
+genericFunction()
+
 
 // Second Rule
+const me = {
+  name: 'Randall',
+  age: 26,
+  intro: 'My name is ',
+  sayMyName: function(name){
+    console.log(this.intro + name);
+  }
+};
+
+me.sayMyName('Randall');
 
 // Third Rule
+function Cat(options){
+  this.name = options.name;
+  this.hasTail = options.hasTail;
+  this.favoriteFood = options.favoriteFood;
+}
+
+const garfield = new Cat({
+    name: 'Garfield',
+    hasTail: true,
+    favoriteFood: 'Lasagna'
+});
+
+Cat.prototype.hungryKitty = function(){
+  console.log(`${this.name} wants ${this.favoriteFood}`);
+};
+
+garfield.hungryKitty();
 
 // Fourth Rule * you may want to use your third rule's example to accomplish this
+const whiskers = new Cat({
+    name: 'Whiskers',
+    hasTail: true,
+    favoriteFood: 'Spaghetti'
+    });
 
-// explain closure 
+whiskers.hungryKitty.call(garfield)
+
+
+// explain closure - An inner function that has access to the variables of enclosing functions.
 
 function foo () {
   console.log(this); // what does this point to?
-};
+}; //This points to the window object
 
 const counterFunction = () => {
-  // this code is broken. figure out why, and tell us where the closure is when you fix it
+  let count = 0;// this code is broken. figure out why, and tell us where the closure is when you fix it - RIGHT HERE
   const changeCount = (value) => {
-    count += value;
+    count += value; //this count value is utilizing closure
   };
   return {
     increment: () => {
@@ -53,15 +94,35 @@ console.log(counter.total());
   // assign these properties you pass in with options to the constructors 'this' object.
   // add a speak() method to your object that when called will log out the car's make model and year.
 
-// when you're done un comment the next few lines and run the file here in node `node app.js`.
 
-// const herby = new Car({make: 'Volkswagen', model: 'Beetle', year: '1963'});
-// console.log(herby.speak());
-// const goldfinger = new Car({make: 'Aston Martin', model: 'DB5', year: '1964'});
-// console.log(goldfinger.speak());
+//First constructor function
+function Car(options){
+  this.make = options.make;
+  this.model = options.model;
+  this.year = options.year;
+}
+Car.prototype.speak = function(){
+console.log(`make: ${this.make}, model: ${this.model}, year: ${this.year}`);
+}
 
+const herby = new Car({make: 'Volkswagen', model: 'Beetle', year: '1963'});
+console.log(herby.speak());
+const goldfinger = new Car({make: 'Aston Martin', model: 'DB5', year: '1964'});
+console.log(goldfinger.speak());
 
-// once you get done with this, redo it all using the class keyword and a constructor function. 
+//es6 constructor with class keyword
+// once you get done with this, redo it all using the class keyword and a constructor function.
+class Car {
+  constructor(options) {
+    this.make = options.make;
+    this.model = options.model;
+    this.year = options.year;
+  }
+  speak() {
+    console.log(`make: ${this.make}, model: ${this.model}, year: ${this.year}`);
+  }
+}
+
 
 // extra credit
 
@@ -76,6 +137,12 @@ while(n >= 1) {
 // write a function called countDown that does the exact same thing as above, but calls itself until it can't anymore.
   // hint-> your base case will look like the logic in the while loop.
 
+const countDown = (n) => {
+  if (n >= 1){
+    console.log(n);
+    n--;
+    countDown(n);
+  }
+}
 
-
-
+countDown(10);
