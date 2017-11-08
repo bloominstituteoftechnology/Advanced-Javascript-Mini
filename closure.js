6c2ebef6fd05e3064092f31359dde1b7aa6035f9
Ryan Hamblin <ryanhca@gmail.com>
2017-11-08 08:26:33 -0700

    re-org

--- closure.js
+++ closure.js
@@ -1,20 +1,3 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-* write out a code example of each explanation above
-*/
-
-// First Principle
-
-// Second Principle
-
-// Third Principle
-
-// Fourth Principle * you may want to use your third rule's example to accomplish this
-
 // explain closure
 
 const counterFunction = () => {
