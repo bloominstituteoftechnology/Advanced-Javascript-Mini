a668b6231682d62c6e32ca4b6d00382d73f18eb2
Ryan Hamblin <ryanhca@gmail.com>
2017-11-08 08:16:08 -0700

    adding step 1

--- app.js
+++ app.js
@@ -14,3 +14,30 @@
 // Third Principle
 
 // Fourth Principle * you may want to use your third rule's example to accomplish this
+
+// explain closure
+
+const counterFunction = () => {
+  // this code is broken. figure out why, and tell us where the closure is when you fix it
+  const changeCount = value => {
+    count += value;
+  };
+  return {
+    increment: () => {
+      changeCount(1);
+    },
+    decrement: () => {
+      changeCount(-1);
+    },
+    total: () => {
+      return count;
+    }
+  };
+};
+const counter = counterFunction();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+console.log(counter.total());
