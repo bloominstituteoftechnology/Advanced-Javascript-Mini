211137d43671b18bdaf0d86416354a1ae53703e8
Ryan Hamblin <ryanhca@gmail.com>
2017-11-08 08:23:18 -0700

    recursion

--- app.js
+++ app.js
@@ -58,3 +58,13 @@ console.log(counter.total());
 // console.log(goldfinger.speak());
 
 // once you get done with this, redo it all using the class keyword and a constructor function.
+
+// Recursion:
+
+let n = 10;
+while (n >= 1) {
+  console.log(n);
+  n--;
+}
+// write a function called countDown that does the exact same thing as above, but calls itself until it can't anymore.
+// hint-> your base case will look like the logic in the while loop.
